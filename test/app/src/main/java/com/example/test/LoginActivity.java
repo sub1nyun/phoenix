a14d12454f693d250c942179d66a53766edc5d76
SeungjooLee123 <77609889+SeungjooLee123@users.noreply.github.com>
2022-03-24 10:47:51 +0900

    가족타이틀

--- test/app/src/main/java/com/example/test/LoginActivity.java
+++ test/app/src/main/java/com/example/test/LoginActivity.java
@@ -105,14 +105,21 @@ public class LoginActivity extends AppCompatActivity {
                     CommonVal.curuser.setPw("a");
 
                     //아기 리스트 불러오기
-                    AskTask task = new AskTask("http://192.168.0.26", "list.bif");
+                    AskTask task = new AskTask(CommonVal.httpip, "list.bif");
                     //로그인 정보로 수정 필요
                     task.addParam("id", CommonVal.curuser.getId());
                     InputStream in = CommonMethod.excuteGet(task);
                     CommonVal.baby_list = gson.fromJson(new InputStreamReader(in), new TypeToken<List<BabyInfoVO>>(){}.getType());
                     CommonVal.curbaby = CommonVal.baby_list.get(0);
 
-                    finish();
+                    //가족정보 불러오기
+                    task = new AskTask(CommonVal.httpip, "titlelist.us");
+                    task.addParam("id", CommonVal.curuser.getId());
+                    in = CommonMethod.excuteGet(task);
+                    CommonVal.family_title = gson.fromJson(new InputStreamReader(in), new TypeToken<List<String>>(){}.getType());
+                    CommonVal.curFamily =  CommonVal.family_title.get(0);
+
+                   finish();
                 }
             }
         });
