e76a42c2125a18167c52bc4a726580b98254c187
eor8451 <77501171+eor8451@users.noreply.github.com>
2022-03-30 09:51:51 +0900

    스플래시 안됨

--- test/app/src/main/java/com/example/test/diary/GraphActivity.java
+++ test/app/src/main/java/com/example/test/diary/GraphActivity.java
@@ -8,6 +8,9 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.test.R;
+import com.example.test.common.AskTask;
+import com.example.test.common.CommonMethod;
+import com.example.test.common.CommonVal;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
@@ -18,17 +21,21 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GraphActivity extends AppCompatActivity{
     TabLayout tab_graph;
-    //TabItem tab_heat, tab_height, tab_weight;
     ImageView back_graph;
-    List<DiaryVO> list_heat;
+    Gson gson = new Gson();
 
     private LineChart lineChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,62 +43,38 @@ public class GraphActivity extends AppCompatActivity{
 
         lineChart = findViewById(R.id.chart1);
         tab_graph = findViewById(R.id.tab_graph);
-        /*tab_heat = findViewById(R.id.tab_heat);
-        tab_height = findViewById(R.id.tab_height);
-        tab_weight = findViewById(R.id.tab_weight);*/
         back_graph = findViewById(R.id.back_graph);
 
+        AskTask task_heat = new AskTask(CommonVal.httpip, "select_heat.stor");
+        task_heat.addParam("baby_id", CommonVal.curbaby.getBaby_id());
+        InputStream in_heat = CommonMethod.excuteGet(task_heat);
+        List<DiaryVO> list_heat = gson.fromJson(new InputStreamReader(in_heat), new TypeToken<List<DiaryVO>>(){}.getType());
+
+        AskTask task_body = new AskTask(CommonVal.httpip, "select_graph.stor");
+        task_body.addParam("baby_id", CommonVal.curbaby.getBaby_id());
+        InputStream in_body = CommonMethod.excuteGet(task_body);
+        List<BabyStorVO> list_body = gson.fromJson(new InputStreamReader(in_body), new TypeToken<List<BabyStorVO>>(){}.getType());
+
         ArrayList<Entry> values = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-
-            float val = (float) (Math.random() * 10);
-            values.add(new Entry(i, val));
-            Log.d("asd", "onCreate: " + values.get(i));
-        }
-
-        LineDataSet lineDataSet = new LineDataSet(values, "속성명1");
-        lineDataSet.setLineWidth(2);
-        lineDataSet.setCircleRadius(6);
-        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
-        lineDataSet.setCircleHoleColor(Color.BLUE);
-        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
-        lineDataSet.setDrawCircleHole(true);
-        lineDataSet.setDrawCircles(true);
-        lineDataSet.setDrawHorizontalHighlightIndicator(false);
-        lineDataSet.setDrawHighlightIndicators(false);
-        lineDataSet.setDrawValues(false);
-        LineData lineData = new LineData(lineDataSet);
-        lineChart.setData(lineData);
-        XAxis xAxis = lineChart.getXAxis(); //x축 설정
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); //x축 위치 설정
-        xAxis.setTextColor(Color.BLACK);
-        xAxis.setLabelCount(5, true);
-        xAxis.enableGridDashedLine(8, 24, 0); //수직 격자선
-        YAxis yLAxis = lineChart.getAxisLeft(); //y축 설정
-        yLAxis.setTextColor(Color.BLACK);
-        YAxis yRAxis = lineChart.getAxisRight();
-        yRAxis.setDrawLabels(false);
-        yRAxis.setDrawAxisLine(false);
-        yRAxis.setDrawGridLines(false);
-        Description description = new Description();
-        description.setText("");
-        lineChart.setDoubleTapToZoomEnabled(false);
-        lineChart.setDrawGridBackground(false);
-        lineChart.setDescription(description);
-        lineChart.setVisibleXRangeMaximum(2); //드래그
-        lineChart.animateY(2000, Easing.EaseInCubic);
-        lineChart.invalidate();
 
         tab_graph.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 if(tab.getPosition() == 0){ //체온
-
+                    values.clear();
+                    for (int i = 0; i < list_heat.size(); i++)
+                        values.add(new Entry(i, (float)list_heat.get(i).getTemperature()));
+                    makeChart(values);
                 } else if(tab.getPosition() == 1){ //키
-
+                    values.clear();
+                    for (int i = 0; i < list_body.size(); i++)
+                        values.add(new Entry(i, (float)list_body.get(i).getStor_cm()));
+                    makeChart(values);
                 } else if(tab.getPosition() == 2){ //몸무게
-
+                    values.clear();
+                    for (int i = 0; i < list_body.size(); i++)
+                        values.add(new Entry(i, (float)list_body.get(i).getStor_kg()));
+                    makeChart(values);
                 }
             }
 
@@ -113,5 +96,41 @@ public class GraphActivity extends AppCompatActivity{
                 finish();
             }
         });
+    }
+
+    public void makeChart(ArrayList<Entry> values){
+        LineDataSet lineDataSet = new LineDataSet(values, "속성명1");
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setCircleRadius(6);
+        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setCircleHoleColor(Color.BLUE);
+        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+        lineDataSet.setDrawHighlightIndicators(false);
+        lineDataSet.setDrawValues(false);
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        XAxis xAxis = lineChart.getXAxis(); //x축 설정
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); //x축 위치 설정
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.mAxisMinimum = 0f;
+        xAxis.setLabelCount(5, true);
+        xAxis.enableGridDashedLine(8, 24, 0); //수직 격자선
+        YAxis yLAxis = lineChart.getAxisLeft(); //y축 설정
+        yLAxis.setTextColor(Color.BLACK);
+        YAxis yRAxis = lineChart.getAxisRight();
+        yRAxis.setDrawLabels(false);
+        yRAxis.setDrawAxisLine(false);
+        yRAxis.setDrawGridLines(false);
+        Description description = new Description();
+        description.setText("");
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription(description);
+        lineChart.setVisibleXRangeMaximum(2); //드래그
+        lineChart.animateY(2000, Easing.EaseInCubic);
+        lineChart.invalidate();
     }
 }
