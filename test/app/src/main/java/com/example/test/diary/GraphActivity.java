fb4bab87b7649c5ba01508d4a9d7daa1a03146cb
eor8451 <77501171+eor8451@users.noreply.github.com>
2022-03-31 14:00:54 +0900

    그래프 1차

--- test/app/src/main/java/com/example/test/diary/GraphActivity.java
+++ test/app/src/main/java/com/example/test/diary/GraphActivity.java
@@ -1,5 +1,6 @@
 package com.example.test.diary;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,12 +23,17 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.DefaultValueFormatter;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
 import com.github.mikephil.charting.interfaces.datasets.IDataSet;
 import com.github.mikephil.charting.renderer.LineChartRenderer;
+import com.github.mikephil.charting.renderer.XAxisRenderer;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Transformer;
+import com.github.mikephil.charting.utils.Utils;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
@@ -36,6 +42,7 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -71,10 +78,11 @@ public class GraphActivity extends AppCompatActivity{
         date.clear();
         values.clear();
         for (int i = 0; i < list_heat.size(); i++) {
-            date.add(list_heat.get(i).getStart_time());
+            date.add(list_heat.get(i).getStart_time() + "," + list_heat.get(i).getDiary_date());
+            //date.add(list_heat.get(i).getStart_time());
             values.add(new Entry(i, (float) list_heat.get(i).getTemperature()));
         }
-        makeChart(values, date);
+        makeChart(values, date, "heat");
 
         tab_graph.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -83,10 +91,11 @@ public class GraphActivity extends AppCompatActivity{
                     date.clear();
                     values.clear();
                     for (int i = 0; i < list_heat.size(); i++) {
-                        date.add(list_heat.get(i).getStart_time());
+                        date.add(list_heat.get(i).getStart_time() + "," + list_heat.get(i).getDiary_date());
+                        //date.add(list_heat.get(i).getStart_time());
                         values.add(new Entry(i, (float) list_heat.get(i).getTemperature()));
                     }
-                    makeChart(values, date);
+                    makeChart(values, date, "heat");
                 } else if(tab.getPosition() == 1){ //키
                     date.clear();
                     values.clear();
@@ -94,7 +103,7 @@ public class GraphActivity extends AppCompatActivity{
                         date.add(list_body.get(i).getStor_date());
                         values.add(new Entry(i, (float) list_body.get(i).getStor_cm()));
                     }
-                    makeChart(values, date);
+                    makeChart(values, date, "cm");
                 } else if(tab.getPosition() == 2){ //몸무게
                     date.clear();
                     values.clear();
@@ -102,7 +111,7 @@ public class GraphActivity extends AppCompatActivity{
                         date.add(list_body.get(i).getStor_date());
                         values.add(new Entry(i, (float)list_body.get(i).getStor_kg()));
                     }
-                    makeChart(values, date);
+                    makeChart(values, date, "kg");
                 }
             }
 
@@ -126,7 +135,8 @@ public class GraphActivity extends AppCompatActivity{
         });
     }
 
-    public void makeChart(ArrayList<Entry> values, ArrayList<String> date){
+    //그래프 생성
+    public void makeChart(ArrayList<Entry> values, ArrayList<String> date, String category){
         LineDataSet lineDataSet = new LineDataSet(values, "라벨 줘야돼?");
         lineDataSet.setLineWidth(2);
         lineDataSet.setCircleRadius(6);
@@ -139,27 +149,49 @@ public class GraphActivity extends AppCompatActivity{
         lineDataSet.setDrawHighlightIndicators(false);
         lineDataSet.setDrawValues(false);
 
-
         LineData lineData = new LineData(lineDataSet);
         lineChart.setData(lineData);
         XAxis xAxis = lineChart.getXAxis(); //x축 설정
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); //x축 위치 설정
+
+        if(category.equals("heat")){
+            String[] diary_date = new String[date.size()];
+            String[] time_date = new String[date.size()];
+
+            for(int i=0; i<date.size(); i++){
+                diary_date[i] = date.get(i).split(",")[1];
+                time_date[i] = date.get(i).split(",")[0];
+            }
+
+            String mdate = diary_date[0];
+            for(int i=1; i<date.size(); i++){
+                if(mdate.equals(diary_date[i])) diary_date[i] = "";
+                else mdate = diary_date[i];
+            }
+
+            xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED); //x축 위치 설정
+
+            //top x axis 설정
+            lineChart.setXAxisRenderer(new DoubleXLabelAxisRenderer(lineChart.getViewPortHandler(), lineChart.getXAxis(), lineChart.getTransformer(YAxis.AxisDependency.LEFT), new IAxisValueFormatter() {
+                @Override
+                public String getFormattedValue(float value, AxisBase axis) {
+                    return diary_date[(int)value];
+                }
+            }));
+
+            //bottom x axis 설정
+            xAxis.setValueFormatter(new IndexAxisValueFormatter(time_date));
+        }else{
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            xAxis.setValueFormatter(new IndexAxisValueFormatter(date)); //x축 표현을 string으로 포맷
+        }
+
         xAxis.setTextColor(Color.BLACK);
         xAxis.mAxisMinimum = 0f;
-        xAxis.setLabelCount(values.size(), false);
+        xAxis.setGranularityEnabled(true); //이거 없으면 x축 데이터 반복되고 포인트랑 안맞음!
+        xAxis.setLabelCount(values.size());
         xAxis.enableGridDashedLine(8, 24, 0); //수직 격자선
         xAxis.mEntryCount = values.size();
         xAxis.setAvoidFirstLastClipping(true);
-
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(date));
-
-        /*xAxis.setValueFormatter(new ValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                String a="";
-                return date.get((int)value);
-            }
-        });*/
 
         YAxis yLAxis = lineChart.getAxisLeft(); //y축 설정
         yLAxis.setTextColor(Color.BLACK);
@@ -170,16 +202,97 @@ public class GraphActivity extends AppCompatActivity{
         /*Description description = new Description();
         description.setText("");*/
 
-        MyMarker mm = new MyMarker(this, R.layout.custom_marker);
+        MyMarker mm = new MyMarker(this, R.layout.custom_marker, category, date);
         mm.setChartView(lineChart);
         lineChart.setMarker(mm);
+
+        lineChart.setExtraLeftOffset(5);
+        lineChart.setExtraRightOffset(20);
+        lineChart.setExtraBottomOffset(20);
+        lineChart.setExtraTopOffset(20);
 
         lineChart.setDoubleTapToZoomEnabled(false);
         lineChart.setDrawGridBackground(false);
         lineChart.setDescription(null);
         lineChart.setVisibleXRangeMaximum(2); //드래그
-        lineChart.animateY(2000, Easing.EaseInCubic);
-        lineChart.getLegend().setEnabled(false);
-        lineChart.invalidate();
+        lineChart.animateY(2000, Easing.EaseInBounce);
+        lineChart.getLegend().setEnabled(false); //속성표시
+        lineChart.invalidate(); //그래프 그리기
+    }
+
+    //위, 아래 x축에 다른 데이터 넣기 위해 필요한 클래스
+    public class DoubleXLabelAxisRenderer extends XAxisRenderer {
+        private IAxisValueFormatter topValueFormatter;
+
+        public DoubleXLabelAxisRenderer(ViewPortHandler viewPortHandler, XAxis xAxis, Transformer transformer, IAxisValueFormatter topValueFormatter) {
+            super(viewPortHandler, xAxis, transformer);
+            this.topValueFormatter = topValueFormatter;
+        }
+        @Override
+        public void renderAxisLabels(Canvas c) {
+            if (!mXAxis.isEnabled() || !mXAxis.isDrawLabelsEnabled()) return;
+
+            float yoffset = mXAxis.getYOffset();
+            mAxisLabelPaint.setTypeface(mXAxis.getTypeface());
+            mAxisLabelPaint.setTextSize(mXAxis.getTextSize());
+            mAxisLabelPaint.setColor(mXAxis.getTextColor());
+
+            MPPointF pointF = MPPointF.getInstance(0, 0);
+            if (mXAxis.getPosition() == XAxis.XAxisPosition.TOP) {
+                pointF.x = 0.5f;
+                pointF.y = 1.0f;
+                drawLabels(c, mViewPortHandler.contentTop() - yoffset, pointF);
+            } else if (mXAxis.getPosition() == XAxis.XAxisPosition.TOP_INSIDE) {
+                pointF.x = 0.5f;
+                pointF.y = 1.0f;
+                drawLabels(c, mViewPortHandler.contentTop() + yoffset + mXAxis.mLabelRotatedHeight, pointF);
+            } else if (mXAxis.getPosition() == XAxis.XAxisPosition.BOTTOM) {
+                pointF.x = 0.5f;
+                pointF.y = 0.0f;
+                drawLabels(c, mViewPortHandler.contentBottom() + yoffset, pointF);
+            } else if (mXAxis.getPosition() == XAxis.XAxisPosition.BOTTOM_INSIDE) {
+                pointF.x = 0.5f;
+                pointF.y = 0.0f;
+                drawLabels(c, mViewPortHandler.contentBottom() - yoffset - mXAxis.mLabelRotatedHeight, pointF);
+            } else { // BOTH SIDED
+                pointF.x = 0.5f;
+                pointF.y = 1.0f;
+                drawLabelsTop(c, mViewPortHandler.contentTop() - yoffset, pointF);
+                pointF.x = 0.5f;
+                pointF.y = 0.0f;
+                drawLabels(c, mViewPortHandler.contentBottom() + yoffset, pointF);
+            }
+            MPPointF.recycleInstance(pointF);
+        }
+
+        private void drawLabelsTop(Canvas c, float pos, MPPointF anchor) {
+            final float labelRotationAngleDegrees = mXAxis.getLabelRotationAngle();
+            boolean centeringEnabled = mXAxis.isCenterAxisLabelsEnabled();
+            float[] positions = new float[mXAxis.mEntryCount * 2];
+
+            for (int i = 0; i < positions.length; i += 2) {
+                if (centeringEnabled) positions[i] = mXAxis.mCenteredEntries[i / 2];
+                else positions[i] = mXAxis.mEntries[i / 2];
+            }
+
+            mTrans.pointValuesToPixel(positions);
+            for (int i = 0; i < positions.length; i += 2) {
+                float x = positions[i];
+
+                if (mViewPortHandler.isInBoundsX(x)) {
+                    String label = topValueFormatter.getFormattedValue(mXAxis.mEntries[i / 2], mXAxis);
+                    if (mXAxis.isAvoidFirstLastClippingEnabled()) {
+                        if (i == mXAxis.mEntryCount - 1 && mXAxis.mEntryCount > 1) {
+                            float width = Utils.calcTextWidth(mAxisLabelPaint, label);
+                            if (width > mViewPortHandler.offsetRight() * 2 && x + width > mViewPortHandler.getChartWidth()) x -= width / 2;
+                        } else if (i == 0) {
+                            float width = Utils.calcTextWidth(mAxisLabelPaint, label);
+                            x += width / 2;
+                        }
+                    }
+                    drawLabel(c, label, x, pos, anchor, labelRotationAngleDegrees);
+                }
+            }
+        }
     }
 }
--- test/app/src/main/java/com/example/test/diary/MyMarker.java
+++ test/app/src/main/java/com/example/test/diary/MyMarker.java
@@ -1,6 +1,7 @@
 package com.example.test.diary;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.test.R;
@@ -10,21 +11,56 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.utils.MPPointF;
 
+import java.util.ArrayList;
+
 public class MyMarker extends MarkerView {
     private TextView tv_marker;
+    String category;
+    ArrayList<String> date;
 
-    public MyMarker(Context context, int layoutResource) {
+    public MyMarker(Context context, int layoutResource, String category, ArrayList<String> date) {
         super(context, layoutResource);
         tv_marker = findViewById(R.id.tv_marker);
+        this.category = category;
+        this.date = date;
     }
 
     @Override
     public void refreshContent(Entry e, Highlight highlight) {
         if(e instanceof CandleEntry){
-            CandleEntry ce = (CandleEntry) e;
-            tv_marker.setText("" + ce.getHigh());
+            if(category.equals("heat")){
+                String[] diary_date = new String[date.size()];
+                String[] time_date = new String[date.size()];
+
+                for(int i=0; i<date.size(); i++){
+                    diary_date[i] = date.get(i).split(",")[1];
+                    time_date[i] = date.get(i).split(",")[0];
+                }
+
+                CandleEntry ce = (CandleEntry) e;
+                Log.d("asd", "refreshContent: " + ce.getX());
+                tv_marker.setText(diary_date[(int)ce.getX()] + " " + time_date[(int)ce.getX()] + "\n" + ce.getHigh());
+            } else if(category.equals("cm")){
+                CandleEntry ce = (CandleEntry) e;
+                tv_marker.setText("" + ce.getHigh());
+            } else if(category.equals("kg")){
+                CandleEntry ce = (CandleEntry) e;
+                tv_marker.setText("" + ce.getHigh());
+            }
+            /*CandleEntry ce = (CandleEntry) e;
+            tv_marker.setText("" + ce.getHigh());*/
         } else{
-            tv_marker.setText("" + e.getY());
+            if(category.equals("heat")){
+                String[] diary_date = new String[date.size()];
+                String[] time_date = new String[date.size()];
+
+                for(int i=0; i<date.size(); i++){
+                    diary_date[i] = date.get(i).split(",")[1];
+                    time_date[i] = date.get(i).split(",")[0];
+                }
+                tv_marker.setText(diary_date[(int)e.getX()] + " " + time_date[(int)e.getX()] + "\n" + e.getY());
+            }
+            //tv_marker.setText("" + e.getY());
         }
         super.refreshContent(e, highlight);
     }
