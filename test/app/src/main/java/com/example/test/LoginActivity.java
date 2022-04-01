34c9861844130b2707d399dc345f69f94b7abfe7
eor8451 <77501171+eor8451@users.noreply.github.com>
2022-04-01 12:23:05 +0900

    a

--- test/app/src/main/java/com/example/test/LoginActivity.java
+++ test/app/src/main/java/com/example/test/LoginActivity.java
@@ -111,8 +111,8 @@ public class LoginActivity extends AppCompatActivity {
                     startActivity(intent);
 
                     //로그인 정보 저장
-                    CommonVal.curuser.setId("deltest");
-                    CommonVal.curuser.setPw("deltest");
+                    CommonVal.curuser.setId("a");
+                    CommonVal.curuser.setPw("a");
 
                     //아기 리스트 불러오기
                     AskTask task = new AskTask(CommonVal.httpip, "list.bif");
