06b3def89004221d684a453bc31f9413f8a23a86
SU BIN <97420319+sub1nyun@users.noreply.github.com>
2022-05-08 22:54:43 +0900

    수빈
    test

--- test/app/src/main/java/com/example/test/MainActivity.java
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -5,7 +5,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.database.Cursor;
 import android.location.LocationManager;
 import android.net.Uri;
@@ -43,6 +45,8 @@ import com.google.android.material.tabs.TabLayout;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class MainActivity extends AppCompatActivity {
@@ -80,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        //getHashKey();
+        getHashKey();
 
         container = findViewById(R.id.container);
         tab_main = findViewById(R.id.tab_main);
@@ -155,6 +159,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", android.util.Base64.encodeToString(md.digest(), android.util.Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
     }
 
     public void changeFrag(Fragment fragment) {
